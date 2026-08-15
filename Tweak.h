3d3cf8412cf61f2d4e6cdce0fefdbac318b4c3c0
hidoxd <alex2108sk@gmail.com>
2026-08-15 22:27:11 +0500

    Update Tweak.h

--- Tweak.h
+++ Tweak.h
@@ -1,12 +1,19 @@
 #import <UIKit/UIKit.h>
-#import <rootless.h>
 #import <objc/runtime.h>
+
+// Безопасное определение пути для Rootless (Dopamine 3.0 / /var/jb)
+#if __has_include(<rootless.h>)
+#import <rootless.h>
+#endif
+
+#ifndef ROOT_PATH_NS
+#define ROOT_PATH_NS(path) [NSString stringWithFormat:@"/var/jb%@", path]
+#endif
 
 #define LSCLOCK_VIEW_TAG 0x15C10C
 #define PREF_PATH ROOT_PATH_NS(@"/Library/Application Support/LSClock/settings.plist")
 #define NOTIFY_PREFS_CHANGED "com.hidoxd.lsclock.prefschanged"
 
-// Макрос для безопасного выполнения UI кода на главном потоке
 #define LS_EXECUTE_ON_MAIN_THREAD(block) \
     if ([NSThread isMainThread]) { \
         block(); \
@@ -14,7 +21,6 @@
         dispatch_async(dispatch_get_main_queue(), block); \
     }
 
-// Конфигурация твика
 typedef struct {
     BOOL enabled;
     BOOL showSeconds;
